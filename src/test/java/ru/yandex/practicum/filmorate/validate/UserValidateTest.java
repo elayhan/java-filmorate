a174d9385eb12a0e28f6cab4f63250dc109c7ccf
e1ayhan <wwwelayhan@yandex.ru>
2023-03-15 00:03:52 +0300

    fix: убрать public для классов тестов

--- src/test/java/ru/yandex/practicum/filmorate/validate/UserValidateTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/validate/UserValidateTest.java
@@ -19,7 +19,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class UserValidateTest {
+class UserValidateTest {
     @Autowired
     private ObjectMapper mapper;
     @Autowired
